71757808d43a33e75785d02e508daa582ef8f1e9
Michael Englbrecht <michael.englbrecht@10point-software.com>
2016-04-21 19:10:35 +0000

    Initial Commit

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,51 @@
+sap.ui.define([
+	'jquery.sap.global',
+	'sap/ui/core/Fragment',
+	'sap/ui/core/mvc/Controller',
+	'sap/ui/model/Filter'
+], function(jQuery, Fragment, Controller, Filter) {
+	"use strict";
+
+	return Controller.extend("sap.training.valuehelp.controller.Main", {
+		handleValueHelp: function(oEvent) {
+			var sInputValue = oEvent.getSource().getValue();
+
+			this.inputId = oEvent.getSource().getId();
+			// create value help dialog
+			if (!this._valueHelpDialog) {
+				this._valueHelpDialog = sap.ui.xmlfragment(
+					"sap.training.valuehelp.view.Dialog2",
+					this
+				);
+				this.getView().addDependent(this._valueHelpDialog);
+			}
+
+			// create a filter for the binding
+			this._valueHelpDialog.getBinding("items").filter([new Filter(
+				"Carrid",
+				sap.ui.model.FilterOperator.Contains, sInputValue
+			)]);
+
+			// open value help dialog filtered by the input value
+			this._valueHelpDialog.open(sInputValue);
+		},
+		_handleValueHelpSearch: function(evt) {
+			var sValue = evt.getParameter("value");
+			var oFilter = new Filter(
+				"Carrid",
+				sap.ui.model.FilterOperator.Contains, sValue
+			);
+			evt.getSource().getBinding("items").filter([oFilter]);
+		},
+
+		_handleValueHelpClose: function(evt) {
+			var oSelectedItem = evt.getParameter("selectedItem");
+			if (oSelectedItem) {
+				var productInput = this.getView().byId(this.inputId);
+				productInput.setValue(oSelectedItem.getTitle());
+			}
+			evt.getSource().getBinding("items").filter([]);
+		}
+	});
+
+});
